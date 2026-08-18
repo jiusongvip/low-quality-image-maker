6883a285a1125b357738fe2fa283db5a2bc0a7cb
DESKTOP-68PFDJP\jiusongPC11 <15795498+yangze9264@user.noreply.gitee.com>
2026-08-18 17:58:18 +0800

    fix: resolve conflict, npx astro build + add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://low-quality-image-maker.com',
   trailingSlash: 'never',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   build: {
     format: 'directory',
   },
